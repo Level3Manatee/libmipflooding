3abcaf75ac489d14b390d11fb356c637d69e2d5b
Jan Ortgies <80638364+Level3Manatee@users.noreply.github.com>
2024-09-24 20:03:56 +0200

    Python wrapper: Add enums header to package

--- wrappers/python/setup.py
+++ wrappers/python/setup.py
@@ -3,5 +3,10 @@ from setuptools import setup, find_packages
 setup(
     packages=["libmipflooding"],
     package_dir={"libmipflooding": "."},
-    package_data={"libmipflooding": ["binaries/libmipflooding.dll", "binaries/libmipflooding.so", "binaries/libmipflooding_extern_c.h"]}
+    package_data={"libmipflooding": [
+        "binaries/libmipflooding.dll",
+        "binaries/libmipflooding.so",
+        "binaries/libmipflooding_extern_c.h",
+        "binaries/libmipflooding_enums.h"
+    ]}
 )
